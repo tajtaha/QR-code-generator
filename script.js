b1b08e57f73bc8e738aeb174bb713e3e2c3f260a
taj <tajprogram710@gmail.com>
2026-02-02 20:01:43 +0330

    Created the logic now the qr code will generate and show a picture just some styling and some debuggings left... and maybe some extra stuff

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const createButton = document.querySelector("#create-button");
 const firstPage = document.querySelector("#first-page");
+const secondPage = document.querySelector("#second-page");
 const logoImage = document.querySelector("#logo-image");
 const header = document.querySelector("#header");
 const input = document.querySelector("#input");
+const qrImage = document.getElementById("qr-image");
+
 createButton.addEventListener("click", function () {
-  if (input.value === "") {
+  const inputValue = input.value.trim();
+  if (!inputValue) {
     alert("input is empty.");
   } else {
     console.log("created the qrcode!");
@@ -18,5 +22,16 @@ createButton.addEventListener("click", function () {
       "mt-4",
     );
     firstPage.classList.add("hidden");
+    secondPage.classList.remove("hidden");
+    QRCode.toDataURL(inputValue, {
+      width: 240,
+      margin: 2,
+    })
+      .then(function (dataUrl1) {
+        qrImage.src = dataUrl1;
+      })
+      .catch(function (err) {
+        alert(`error: ${err}`);
+      });
   }
 });
